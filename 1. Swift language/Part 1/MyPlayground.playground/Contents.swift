d97355b2e30e1ebc5c63aa63e9db7a2e444bcd00
arturbednarz <slliver@icloud.com>
2025-05-16 17:30:32 +0200

    Chapter Swift, Part 1, Task 1 completed

--- 1. Swift language/Part 1/MyPlayground.playground/Contents.swift
+++ 1. Swift language/Part 1/MyPlayground.playground/Contents.swift
@@ -1,3 +1,27 @@
-import UIKit
+public func isPalindrome(input: String) -> Bool {
+    func isControlCharacter(_ char: Character) -> Bool {
+        guard let scalar = char.unicodeScalars.first else { return false }
+        let value = scalar.value
+        return (value >= 0x00 && value <= 0x1F) || (value >= 0x80 && value <= 0x9F) || value == 0x7F
+    }
 
-var greeting = "Hello, playground"
+    let lowerInput = input.lowercased()
+    var cleanChars: [Character] = []
+    
+    for char in lowerInput{
+        if !char.isWhitespace &&
+            !char.isPunctuation &&
+            !isControlCharacter(char){
+            cleanChars.append(char)
+        }
+    }
+    
+    let cleanString = String(cleanChars)
+    
+    if cleanString.count <= 1{
+        return false
+    }
+    
+    return cleanString == String(cleanString.reversed())
+}
+
