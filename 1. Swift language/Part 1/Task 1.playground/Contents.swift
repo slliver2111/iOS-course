42cef83ff571b0fa25de35fe36191279b57d82ed
arturbednarz <slliver@icloud.com>
2025-05-17 16:26:45 +0200

    Add coments to part 1

--- 1. Swift language/Part 1/Task 1.playground/Contents.swift
+++ 1. Swift language/Part 1/Task 1.playground/Contents.swift
@@ -1,3 +1,14 @@
+//Implement Palindrome Check Using String Manipulation. Create a function isPalindrome(input: String) -> Bool that checks whether a given string is a palindrome:
+//
+//
+//public func isPalindrome(input: String) -> Bool {
+//// Add you implmentation here
+//return false
+//}
+// However, the trick is:
+//- The solution should ignore spaces, punctuation, capitalization and control characters.
+//- The solution should not use any third-party libraries or regular expressions.
+//- The solution should not consider an empty string or a single character as a palindrome.
 public func isPalindrome(input: String) -> Bool {
     // Helper function to identify C0, C1, and DEL control characters.
     func isControlCharacter(_ char: Character) -> Bool {
@@ -9,6 +20,7 @@ public func isPalindrome(input: String) -> Bool {
     let lowerInput = input.lowercased()
     var cleanChars: [Character] = []
     
+    // Clean string by removing bad characters
     for char in lowerInput{
         if !char.isWhitespace &&
             !char.isPunctuation &&
@@ -19,10 +31,12 @@ public func isPalindrome(input: String) -> Bool {
     
     let cleanString = String(cleanChars)
     
+    // If there is only 1 character or nothing return false
     if cleanString.count <= 1{
         return false
     }
     
+    // After cleaning if it is palindrome reversed string should match
     return cleanString == String(cleanString.reversed())
 }
 
--- 1. Swift language/Part 1/Task 2.playground/Contents.swift
+++ 1. Swift language/Part 1/Task 2.playground/Contents.swift
@@ -1,13 +1,25 @@
+//Write a function isBalancedParentheses(input: String) -> Bool that returns true if parentheses in a string are balanced, and false otherwise.
+//public func isBalancedParentheses(input: String) -> Bool {
+// Add you implmentation here
+//return false
+//}
+//
+//
+//- Only parentheses () are considered; other brackets or characters should be ignored.
+//- The input string might contain multiple lines.
+//- For example, "(())" is balanced, but "(()" is not.
 public func isBalancedParentheses(input: String) -> Bool {
     var openParentCount: Int = 0
     var closeParentCount: Int = 0
     
+    // Iterate over string and count number of '(' and ')'.
     for char in input {
         switch char {
         case "(":
             openParentCount += 1
         case ")":
             closeParentCount += 1
+            // Anytime ')' occurs more often return false
             guard closeParentCount <= openParentCount else {
                 return false
             }
@@ -15,5 +27,6 @@ public func isBalancedParentheses(input: String) -> Bool {
             continue
         }
     }
+    // Number of '(' and ')' should be the same
     return openParentCount == closeParentCount
 }
