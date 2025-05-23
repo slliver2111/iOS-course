0a13bc50f73a6577d4ca7f277ae9fe8f000b55cc
arturbednarz <slliver@icloud.com>
2025-05-23 14:58:52 +0200

    Chapter Swift, Part 2, Task 3 ready to send

--- 1. Swift language/Part 2/Task 3 University Student Management System – Extended Version.playground/Contents.swift
+++ 1. Swift language/Part 2/Task 3 University Student Management System – Extended Version.playground/Contents.swift
@@ -1,33 +1,4 @@
-//What should be done
-//*
-//Steps to Implement:
-//
-//1. Modify Person class:
-//
-//• Add a computed property (isAdult: Bool) which check age >= 18
-//
-//• Add a static property (minAgeForEnrollment = 16)
-//
-//• Add a lazy property (profileDescription) which return "\(name) is \(age) years old."
-//
-//2. Modify Student subclass:
-//
-//• Add a static counter to track the number of students - studentCount
-//
-//• Add a weak reference to an optional Professor (advisor)
-//
-//• Add a computed property to format studentID - formattedID, which return "ID: " and studentID in uppercased
-//
-//3. Modify Professor subclass:
-//
-//• Add a static counter for professors - professorCount
-//
-//• Add a computed property for fullTitle
-//
-//4. Modify University struct:
-//
-//• Add a computed property (description)
-
+// A counter for tracking Student instances
 actor StudentCounter {
     var counter = 0
     
@@ -35,16 +6,25 @@ actor StudentCounter {
         counter += 1
     }
     
+    func decrement() {
+        counter -= 1
+    }
+    
     func getValue() -> Int {
         return counter
     }
 }
 
+// A counter for tracking Professor instances
 actor ProfessorCounter {
     var counter = 0
     
     func increment() {
         counter += 1
+    }
+    
+    func decrement() {
+        counter -= 1
     }
     
     func getValue() -> Int {
@@ -60,7 +40,7 @@ class Person {
     }
     static let minAgeForEnrollment = 16
     lazy var profileDescription: String = {
-        return "\(name) is \(age) years old."
+        return "\(self.name) is \(self.age) years old."
     }()
     
     // A designated initializer
@@ -71,8 +51,8 @@ class Person {
     
     // A failable initializer (fails if age is less than 16)
     init?(name: String, ageCheck age: Int) {
-        guard age >= 16 else {
-            print("Person init failed. Person need to be >= 16")
+        guard age >= Person.minAgeForEnrollment else {
+            print("Person init failed. Person need to be >= \(Person.minAgeForEnrollment)")
             return nil
         }
         self.name = name
@@ -97,14 +77,18 @@ class Student: Person {
     required init(name: String, age: Int, studentID: String) {
         self.studentID = studentID
         super.init(name: name, age: age)
-        //studentCounter.increment()
+        Task{
+            await studentCounter.increment()
+        }
     }
     
     // Cover ageCheck feature
     init?(name: String, ageCheck age: Int, studentID: String) {
         self.studentID = studentID
         super.init(name: name, ageCheck: age)
-        //studentCounter.increment()
+        Task{
+            await studentCounter.increment()
+        }
     }
     
     // A convenience initializer
@@ -117,6 +101,13 @@ class Student: Person {
     convenience init?(name: String, ageCheck age: Int, studentID: String, major: String) {
         self.init(name: name, ageCheck: age, studentID: studentID)
         self.major = major
+    }
+    
+    deinit {
+        Task{
+            await studentCounter.decrement()
+            print("Student removed")
+        }
     }
 }
 
@@ -136,23 +127,27 @@ class Professor: Person {
     init(name: String, age: Int, faculty: String) {
         self.faculty = faculty
         super.init(name: name, age: age)
-        //professorCounter.increment()
+        Task{
+            await professorCounter.increment()
+        }
     }
     
     // Cover ageCheck feature
     init?(name: String, ageCheck age: Int, faculty: String) {
         self.faculty = faculty
         super.init(name: name, ageCheck: age)
-        //professorCounter.increment()
-        
+        Task{
+            await professorCounter.increment()
+        }
+    }
+    
+    deinit {
+        Task{
+            await professorCounter.decrement()
+            print("Professor removed")
+        }
     }
 }
-
-//class MedicineStudent: Student {
-//    required init(name: String, age: Int, studentID: String) {
-//        super.init(name: name, age: age, studentID: studentID)
-//    }
-//}
 
 // A memberwise initializer (default in Swift for structs)
 struct University{
@@ -163,20 +158,49 @@ struct University{
     }
 }
 
+// Global instance of counter actor
+let studentCounter = StudentCounter()
+let professorCounter = ProfessorCounter()
+
+// TEST
 let artur = Person(name: "Artur", age: 10)
 let ola = Person(name: "Ola", age: 22)
 let tomek = Person(name: "Tomek", ageCheck: 19)
 let lukas = Person(name: "Lukas", ageCheck: 10) // Expected nil
 let maciek = Student(name: "Maciek", age: 20, studentID: "01")
 let marta = Student(name: "Marta", age: 20, studentID: "02", major: "IT")
-let marek = Student(name: "Marek", ageCheck: 20, studentID: "00")
+var marek = Student(name: "Marek", ageCheck: 20, studentID: "00")
 let romek = Student(name: "Romek", ageCheck: 10, studentID: "03") // Expected nil
 let weronika = Student(name: "Weronika", ageCheck: 10, studentID: "04", major: "Medicine") // Expected nil
 
-let pawel = Professor(name: "Pawel", age: 22, faculty: "History")
+var pawel = Professor(name: "Pawel", ageCheck: 22, faculty: "History")
 let prz = University(name: "Prz", location: "Rzeszow")
 
-// Global instance of counter actor
-let studentCounter = StudentCounter()
-let professorCounter = ProfessorCounter()
+// TEST part 2
+print(artur.isAdult)
+print(ola.isAdult)
 
+print(Student.minAgeForEnrollment)
+print(ola.profileDescription)
+print(maciek.profileDescription)
+
+Task{ //async
+    print("Number of students \(await Student.studentCount())") //Expected 3
+}
+
+Task{ //async
+    try await Task.sleep(for: .seconds(3)) //Attempt to allow deinit task to be completed
+    marek = nil
+    try await Task.sleep(for: .milliseconds(100)) //Give short additional time
+    print("Number of students \(await Student.studentCount())") // Expected 2
+}
+
+maciek.advisor = pawel
+print(maciek.advisor?.fullTitle ?? "No advisor") // // Expected Prof. Hist Pawel
+
+pawel = nil
+print(maciek.advisor?.fullTitle ?? "No advisor") // Expected no advisor
+print(marta.advisor?.fullTitle ?? "No advisor") // Expected no advisor
+
+print(marta.formattedID)
+print(prz.description)
