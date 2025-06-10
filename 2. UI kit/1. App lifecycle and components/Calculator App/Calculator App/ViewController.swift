693735d745d00d0f68e25462e443bcf53dd30c8e
arturbednarz <slliver@icloud.com>
2025-06-10 20:21:58 +0200

    Calculator: new layout

--- 2. UI kit/1. App lifecycle and components/Calculator App/Calculator App/ViewController.swift
+++ 2. UI kit/1. App lifecycle and components/Calculator App/Calculator App/ViewController.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import Foundation
 
 class ViewController: UIViewController {
     override func viewDidLoad() {
@@ -15,28 +14,25 @@ class ViewController: UIViewController {
         createCalculatorButtons()
     }
     
-    let mainLabel = UILabel()
+    private let mainLabel = UILabel()
     
-    let gridButtons: [Int: String] = [
-        1: "1", 2: "2", 3: "3", 4: "/",
-        5: "4", 6: "5", 7: "6", 8: "*",
-        9: "7", 10: "8", 11: "9", 12: "-",
-        13: "CLR", 14: "0", 15: "=", 16: "+",
+    private let gridButtons: [String] = [
+        "7", "8", "9", "/",
+        "4", "5", "6", "*",
+        "1", "2", "3", "-",
+        "C", "0", "=", "+"
     ]
     
     func createCalculatorButtons() {
-        let rawWidth: Double = 90.0
-        let rawHeight: Double = 100.0
+        let width: Double = 90.0
+        let height: Double = 100.0
         let paddingx: Double = 1.0
         let paddingy: Double = 1.0
         let offsetx: Double = 10.0
         let offsety: Double = 300.0
         
-        let width: CGFloat = Double(rawWidth)
-        let height: CGFloat = Double(rawHeight)
-        
         // Customize main label
-        mainLabel.frame = CGRect(x: offsetx, y: offsety, width: 4 * rawWidth + 3 * paddingy, height: rawHeight)
+        mainLabel.frame = CGRect(x: offsetx, y: offsety, width: 4 * width + 3 * paddingy, height: height)
         mainLabel.text = "0"
         mainLabel.textColor = .systemBlue
         mainLabel.font = UIFont.systemFont(ofSize: 24, weight: .medium)
@@ -48,16 +44,11 @@ class ViewController: UIViewController {
         // Create buttons grid
         for i in 0...3 {
             for j in 0...3 {
-                let x: CGFloat = Double(Double(j) * (rawWidth + paddingx) + offsetx)
-                let y: CGFloat = Double(Double(i) * (rawHeight + paddingy) + offsety + rawHeight + paddingy)
+                let x: CGFloat = Double(j) * (width + paddingx) + offsetx
+                let y: CGFloat = Double(i) * (height + paddingy) + offsety + height + paddingy
                 let button = UIButton(frame: CGRect(x: x, y: y, width: width, height: height))
-                
-                let gridLocation = i*4+j+1
-                if let title = gridButtons[gridLocation] {
-                    button.setTitle(title, for: .normal)
-                    button.tag = gridLocation
-                }
-                
+                button.setTitle(gridButtons[i*4+j], for: .normal)
+                button.tag = i*4+j+1
                 button.setTitleColor(.white, for: .normal)
                 button.backgroundColor = .systemBlue
                 button.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
@@ -66,35 +57,47 @@ class ViewController: UIViewController {
         }
     }
     
-    @objc func buttonTapped(_ sender: UIButton) {
-        // Delete action
-        if sender.tag == 13 && mainLabel.text?.isEmpty == false {
-            mainLabel.text = "0"
-        }
-        
-        // Calculate action
-        else if sender.tag == 15, let input = mainLabel.text {
-            let expression = Expression(input)
-            do {
-                let doubleResult = try expression.evaluate() // 11
-                if doubleResult.isInfinite {
-                    mainLabel.text = "Undefined"
-                } else {
-                    let intResult = Int(doubleResult)
-                    if Double(intResult) == doubleResult {
-                        mainLabel.text = String(intResult)
-                    } else {
-                        mainLabel.text = String(doubleResult)
-                    }
-                }
-            } catch {
-                print("Error")
-            }
-        }
-        
-        // Other action
-        else if let newText = gridButtons[sender.tag], let oldText = mainLabel.text {
-            mainLabel.text = ((oldText == "0" || oldText == "Undefined") ? "" : oldText) + newText
-        }
+    private var operatorToUse: OperationType = .idle
+    private var firstNumberToUse: Int?
+    private var secondNumberToUse: Int?
+    
+    enum OperationType {
+        case add, sub, multi, divide, idle
     }
+    
+    @objc func buttonTapped(_ sender: UIButton) {
+        
+        
+    }
+    
+//    @objc func buttonTapped(_ sender: UIButton) {
+//        // Delete action
+//        if sender.tag == 13 && mainLabel.text?.isEmpty == false {
+//            mainLabel.text = "0"
+//            operatorToUse = .idle
+//            firstNumberToUse = nil
+//            secondNumberToUse = nil
+//        }
+//        
+//        // Calculate action using Expression framework
+//        else if sender.tag == 15, let num1 = firstNumberToUse, let num2 = secondNumberToUse  {
+//            switch operatorToUse {
+//            case .add:
+//                mainLabel.text = String(firstNumberToUse! + secondNumberToUse!)
+//            case .sub:
+//                mainLabel.text = String(firstNumberToUse! - secondNumberToUse!)
+//            case .multi:
+//                mainLabel.text = String(firstNumberToUse! * secondNumberToUse!)
+//            case .divide:
+//                mainLabel.text = String(Double(firstNumberToUse!) / Double(secondNumberToUse!))
+//            case .idle:
+//                print("test")
+//            }
+//        }
+//        
+//        // Other normal action
+//        else if let newText = gridButtons[sender.tag], let oldText = mainLabel.text {
+//            mainLabel.text = ((oldText == "0" || oldText == "Undefined") ? "" : oldText) + newText
+//        }
+//    }
 }
