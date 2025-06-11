9e648682736d98b53861f842fe231c9816f654b6
arturbednarz <slliver@icloud.com>
2025-06-11 16:03:14 +0200

    Autolayout: task 2 done

--- 2. UI kit/2. Autolayout/AutolatoutPracticalTasks/AutolatoutPracticalTasks/Tasks/Task2ViewController.swift
+++ 2. UI kit/2. Autolayout/AutolatoutPracticalTasks/AutolatoutPracticalTasks/Tasks/Task2ViewController.swift
@@ -1,21 +1,59 @@
-//
-//  Task2.swift
-//  AutolatoutPracticalTasks
-//
-//  Created by Kakhaberi Kiknadze on 20.03.25.
-//
-
 import UIKit
 
 // Build a UI programmatically with a UIButton positioned below a UILabel.
 // The button should be centered horizontally and have a fixed distance from the label.
 // Adjust the layout to handle different screen sizes.
 final class Task2ViewController: UIViewController {
+    private let titleLabel = UILabel()
+    private let myButton = UIButton(type: .system)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
     }
-}
-
-#Preview {
-    Task2ViewController()
+    
+    
+    private func setupView() {
+        setupLabel()
+        setupButton()
+        setupConstraints()
+    }
+    
+    
+    private func setupLabel() {
+        titleLabel.text = "My label"
+        titleLabel.font = .boldSystemFont(ofSize: 32)
+        view.addSubview(titleLabel)
+    }
+    
+    private func setupButton() {
+        myButton.setTitle("My button", for: .normal)
+        myButton.setTitleColor(.white, for: .normal)
+        myButton.backgroundColor = .systemBlue
+        view.addSubview(myButton)
+    }
+    
+    private func setupConstraints() {
+        setupLabelsConstraints()
+        setupButtonConstraints()
+    }
+    
+    private func setupLabelsConstraints() {
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate(
+            [
+                titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+                titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            ]
+        )
+    }
+    
+    
+    private func setupButtonConstraints() {
+        myButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            myButton.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20),
+            myButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
 }
