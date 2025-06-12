d6df640b3e9f7215ee0c9301a2c7f913d71365b3
arturbednarz <slliver@icloud.com>
2025-06-12 11:41:11 +0200

    Autolayout: task 3 done

--- 2. UI kit/2. Autolayout/AutolatoutPracticalTasks/AutolatoutPracticalTasks/Tasks/Task3ViewController.swift
+++ 2. UI kit/2. Autolayout/AutolatoutPracticalTasks/AutolatoutPracticalTasks/Tasks/Task3ViewController.swift
@@ -43,9 +43,25 @@ final class Task3ViewController: UIViewController {
     }
     
     private func setupView() {
+        setupContentView()
         setupLabels()
         setupTextFields()
         setupButton()
+        setupConstraints()
+    }
+    
+    private func setupContentView() {
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(contentView)
+        
+        NSLayoutConstraint.activate(
+            [
+                contentView.topAnchor.constraint(greaterThanOrEqualTo: view.topAnchor),
+                contentView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                contentView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                contentView.bottomAnchor.constraint(lessThanOrEqualTo: view.keyboardLayoutGuide.topAnchor)
+            ]
+        )
     }
     
     private func setupLabels() {
@@ -56,6 +72,9 @@ final class Task3ViewController: UIViewController {
         Lorem ipsum dolor sit amet, consectetur adipiscing elit,
         sed do eiusmod tempor incididunt ut labore
         """
+        
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(bodyLabel)
     }
     
     private func setupTextFields() {
@@ -63,11 +82,61 @@ final class Task3ViewController: UIViewController {
         passwordField.placeholder = "Enter password"
         usernameField.borderStyle = .roundedRect
         passwordField.borderStyle = .roundedRect
+        
+        contentView.addSubview(usernameField)
+        contentView.addSubview(passwordField)
     }
     
     private func setupButton() {
         logInButton.setTitle("Log In", for: .normal)
         logInButton.setTitleColor(.tintColor, for: .normal)
+        contentView.addSubview(logInButton)
+    }
+    
+    private func setupConstraints() {
+        setupLabelsConstraints()
+        setupTextFieldsConstraints()
+        setupButtonConstraints()
+    }
+    
+    private func setupLabelsConstraints() {
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        bodyLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate(
+            [
+                titleLabel.topAnchor.constraint(greaterThanOrEqualTo: contentView.topAnchor),
+                titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+                titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+                bodyLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 12),
+                bodyLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+                bodyLabel.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor),
+            ]
+        )
+    }
+    
+    private func setupTextFieldsConstraints() {
+        usernameField.translatesAutoresizingMaskIntoConstraints = false
+        passwordField.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            usernameField.topAnchor.constraint(equalTo: bodyLabel.bottomAnchor, constant: 40),
+            usernameField.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            contentView.trailingAnchor.constraint(equalTo: usernameField.trailingAnchor, constant: 16),
+            passwordField.topAnchor.constraint(equalTo: usernameField.bottomAnchor, constant: 16),
+            passwordField.leadingAnchor.constraint(equalTo: usernameField.leadingAnchor),
+            passwordField.trailingAnchor.constraint(equalTo: usernameField.trailingAnchor)
+        ])
+    }
+    
+    private func setupButtonConstraints() {
+        logInButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            logInButton.topAnchor.constraint(equalTo: passwordField.bottomAnchor, constant: 16),
+            logInButton.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            contentView.trailingAnchor.constraint(equalTo: logInButton.trailingAnchor, constant: 16),
+            logInButton.bottomAnchor.constraint(lessThanOrEqualTo: contentView.bottomAnchor)
+        ])
     }
 }
 
