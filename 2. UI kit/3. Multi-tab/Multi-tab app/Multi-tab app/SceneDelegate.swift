894e504df403862d95703236b4f3de2249efee91
arturbednarz <slliver@icloud.com>
2025-06-15 23:19:53 +0200

    Multitab app: init

---
+++ 2. UI kit/3. Multi-tab/Multi-tab app/Multi-tab app/SceneDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  SceneDelegate.swift
+//  Multi-tab app
+//
+//  Created by Artur Bednarz on 15/06/2025.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        // Create a new window
+        let window = UIWindow(windowScene: windowScene)
+
+        // Create an instance of your ViewController
+        let viewController = ViewController()
+
+        // Set the root view controller and make the window visible
+        window.rootViewController = viewController
+        self.window = window
+        window.makeKeyAndVisible()
+    }
+}
+
---
+++ 2. UI kit/3. Multi-tab/Multi-tab app/Multi-tab app/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Multi-tab app
+//
+//  Created by Artur Bednarz on 15/06/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGray
+    }
+}
