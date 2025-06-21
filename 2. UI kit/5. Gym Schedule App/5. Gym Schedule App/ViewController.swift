aa788e5713c375fcaba807faca81bcbcd9add6bd
arturbednarz <slliver@icloud.com>
2025-06-21 21:23:04 +0200

    Gym app: init

---
+++ 2. UI kit/5. Gym Schedule App/5. Gym Schedule App/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  5. Gym Schedule App
+//
+//  Created by Artur Bednarz on 21/06/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
