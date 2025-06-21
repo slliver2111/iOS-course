5bed37628acb291de5b3434253635e08e6eabda2
arturbednarz <slliver@icloud.com>
2025-06-21 11:44:52 +0200

    Image display: init

---
+++ 2. UI kit/4. Image Display/4. Image Display/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  4. Image Display
+//
+//  Created by Artur Bednarz on 21/06/2025.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        
+        window.rootViewController = ViewController()
+        self.window = window
+        window.makeKeyAndVisible()
+    }
+}
