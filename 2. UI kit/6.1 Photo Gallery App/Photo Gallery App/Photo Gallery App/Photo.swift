42cabf8ad9ac60c96812bb238b8d737a81accd48
arturbednarz <slliver@icloud.com>
2025-07-02 11:10:18 +0200

    Fix: Correct directory case sensitivity

---
+++ 2. UI kit/6.1 Photo Gallery App/Photo Gallery App/Photo Gallery App/Photo.swift
@@ -0,0 +1,31 @@
+//
+//  Photo.swift
+//  Photo Gallery App
+//
+//  Created by Artur Bednarz on 28/06/2025.
+//
+
+import UIKit
+import Foundation
+
+struct Photo {
+    let title: String
+    let date: Date
+    var isFavorite: Bool = false
+    let image: UIImage?
+    
+    static func createExampleArray() -> [Self] {
+        var newArrayOfPhotos: [Self] = []
+        for i in 1...6 {
+            let newPhoto = Photo(title: "Example title \(String(i))", date: Date.now, isFavorite: false, image: UIImage(named: String(i)))
+            newArrayOfPhotos.append(newPhoto)
+            
+        }
+        for i in 7...11 {
+            let newPhoto = Photo(title: "Example title \(String(i))", date: Calendar.current.date(from: DateComponents(year: 2022))!, isFavorite: true, image: UIImage(named: String(i)))
+            newArrayOfPhotos.append(newPhoto)
+            
+        }
+        return newArrayOfPhotos
+    }
+}
