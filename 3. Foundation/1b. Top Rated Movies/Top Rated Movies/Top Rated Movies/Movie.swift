f2b8fa39f0c5f2d1277fcff10c89d3d0b9cb280c
arturbednarz <slliver@icloud.com>
2025-07-08 09:15:45 +0200

    Top movies: finished, small polish needed

---
+++ 3. Foundation/1b. Top Rated Movies/Top Rated Movies/Top Rated Movies/Movie.swift
@@ -0,0 +1,8 @@
+//
+//  Movie.swift
+//  Top Rated Movies
+//
+//  Created by Artur Bednarz on 07/07/2025.
+//
+
+import Foundation
---
+++ 3. Foundation/1b. Top Rated Movies/Top Rated Movies/Top Rated Movies/MovieCell.swift
@@ -0,0 +1,18 @@
+//
+//  MovieCell.swift
+//  Top Rated Movies
+//
+//  Created by Artur Bednarz on 07/07/2025.
+//
+
+import SwiftUI
+
+struct MovieCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieCell()
+}
