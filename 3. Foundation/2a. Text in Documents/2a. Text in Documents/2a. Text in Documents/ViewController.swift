67120a601398e9ac067fb059705895afbe6c57cf
arturbednarz <slliver@icloud.com>
2025-07-12 12:46:20 +0200

    Text in documents app: finished

--- 3. Foundation/2a. Text in Documents/2a. Text in Documents/2a. Text in Documents/ViewController.swift
+++ 3. Foundation/2a. Text in Documents/2a. Text in Documents/2a. Text in Documents/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
         let tv = UITextView()
         tv.layer.borderWidth = 1
         tv.font = .systemFont(ofSize: 16)
+        tv.textContainerInset = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
         return tv
     }()
     
@@ -20,6 +21,7 @@ class ViewController: UIViewController {
         button.backgroundColor = .systemBlue
         button.setTitleColor(.white, for: .normal)
         button.setTitle("Save Text", for: .normal)
+        button.layer.cornerRadius = 8
         return button
     }()
     
@@ -28,6 +30,7 @@ class ViewController: UIViewController {
         button.backgroundColor = .systemBlue
         button.setTitleColor(.white, for: .normal)
         button.setTitle("Load Text", for: .normal)
+        button.layer.cornerRadius = 8
         return button
     }()
     
@@ -39,21 +42,23 @@ class ViewController: UIViewController {
         return stack
     }()
     
-    private let fileName: String = "test.txt"
+    private let fileName: String = "text_file.txt"
     private var fileURL: URL? {
-        FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent(fileName)
+        guard let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {return nil}
+        return documentsDirectory.appendingPathComponent(fileName)
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+        setupActions()
+        loadText(AlertToShow: false)
+    }
+    
+    private func setupUI() {
         self.title = "Text to/from Documents"
         view.backgroundColor = .systemBackground
-        //saveText("Hello, I wish to become ios developer", to: "test.txt")
-        
         view.addSubview(mainStack)
-        
-        saveButton.addTarget(self, action: #selector(saveText), for: .touchUpInside)
-        loadButton.addTarget(self, action: #selector(loadText), for: .touchUpInside)
         
         NSLayoutConstraint.activate([
             mainStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
@@ -64,13 +69,25 @@ class ViewController: UIViewController {
         ])
     }
     
+    private func setupActions() {
+        saveButton.addTarget(self, action: #selector(saveButtonTapped), for: .touchUpInside)
+        loadButton.addTarget(self, action: #selector(loadButtonTapped), for: .touchUpInside)
+    }
+    
     private func showAlert(title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default))
         present(alert, animated: true)
     }
+    
+    @objc private func saveButtonTapped() {
+        saveText()
+    }
+    @objc private func loadButtonTapped() {
+        loadText(AlertToShow: true)
+    }
 
-    @objc private func saveText() {
+    private func saveText() {
         let text = textView.text ?? "no text entered"
         guard let url = fileURL else {return}
         
@@ -78,19 +95,23 @@ class ViewController: UIViewController {
             try text.write(to: url, atomically: true, encoding: .utf8)
             showAlert(title: "Success", message: "Text saved!")
         } catch {
-            showAlert(title: "Error", message: "Failed to save text: \(error)")
+            showAlert(title: "Error", message: "Failed to save the text: \(error.localizedDescription)")
         }
     }
     
-    @objc private func loadText() {
-        guard let url = fileURL else {return}
-        
+    private func loadText(AlertToShow: Bool) {
+        guard let url = fileURL, FileManager.default.fileExists(atPath: url.path) else {
+            if AlertToShow {
+                showAlert(title: "Not found", message: "Saved text file not found")
+            }
+            return
+        }
         do {
             let text = try String(contentsOf: url, encoding: .utf8)
             textView.text = text
-            showAlert(title: "Success", message: "Text loaded seccessfuly!")
+            showAlert(title: "Success", message: "Text loaded seccessfully!")
         } catch {
-            showAlert(title: "Error", message: "Failed to save text: \(error)")
+            showAlert(title: "Error", message: "Failed to load the text: \(error.localizedDescription)")
         }
     }
 }
