17618ec0f2a5322fc76c40760914fcf609905696
arturbednarz <slliver@icloud.com>
2025-07-28 14:40:51 +0200

    Task 1: extension code

--- 3. Foundation/4a. Task 1/Task 1.playground/Contents.swift
+++ 3. Foundation/4a. Task 1/Task 1.playground/Contents.swift
@@ -1,34 +1,39 @@
 import Foundation
 
-actor Counter {
+class Counter: @unchecked Sendable {
     var value = 0
-    
+    private let lock = NSLock()
+
     func increment() {
+        lock.lock()
         value += 1
+        lock.unlock()
     }
 }
 
-func runCounterTask() async {
+func runCounterTask() {
     let counter = Counter()
-    
-    await withTaskGroup(of: Void.self) { group in
-        group.addTask {
-            for _ in 1...1000 {
-                await counter.increment()
-            }
-        }
-        
-        group.addTask {
-            for _ in 1...1000 {
-                await counter.increment()
-            }
+
+    let thread1 = Thread {
+        for _ in 1...1000 {
+            counter.increment()
         }
     }
-    
-    let finalValue = await counter.value
-    print("Final counter value: \(finalValue) (Expected: 2000)")
+
+    let thread2 = Thread {
+        for _ in 1...1000 {
+            counter.increment()
+        }
+    }
+
+    thread1.start()
+    thread2.start()
+
+    while thread1.isExecuting || thread2.isExecuting {
+        usleep(100)
+    }
+
+    print("Final counter value: \(counter.value) (Expected: 2000, but will likely be incorrect)")
 }
 
-Task {
-    await runCounterTask()
-}
+runCounterTask()
