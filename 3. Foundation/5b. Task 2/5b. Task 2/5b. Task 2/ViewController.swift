5f7506b4484c9fdba2861d91d029e47412e2c9c8
arturbednarz <slliver@icloud.com>
2025-07-28 17:35:53 +0200

    Task 2 queue answers

--- 3. Foundation/5b. Task 2/5b. Task 2/5b. Task 2/ViewController.swift
+++ 3. Foundation/5b. Task 2/5b. Task 2/5b. Task 2/ViewController.swift
@@ -33,6 +33,8 @@ class ViewController: UIViewController {
         
         // MARK: 4 exercise
         oprA.queuePriority = .low
+        oprB.addDependency(oprC)
+        oprD.addDependency(oprB)
         operationQueue.addOperations([oprA, oprB, oprC, oprD], waitUntilFinished: false)
     }
     
@@ -48,3 +50,7 @@ class ViewController: UIViewController {
     
 }
 
+//Case 1: Start of operations in the order. Random finish of them.
+//Case 2: Only 2 operations are allowed to work at the same time. Once a slot is free, another operation is started.
+//Case 3: Only operations C and A are allowed to begin. Once the C is completed the B starts operation. Same with operations D. As result operation D is the very last one to start and finish.
+//Case 4: With .low queue priority operations A seems to be lazy. Everytime this test was performed operation A started after operation C is finished and B was started.
