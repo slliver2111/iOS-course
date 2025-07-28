8c96484820f716926f2230e9510b2f64db45d6fc
arturbednarz <slliver@icloud.com>
2025-07-28 16:18:24 +0200

    Task1 OperationQueue fixes

--- 3. Foundation/5a. Task 1/5a. Task 1/5a. Task 1/ViewController.swift
+++ 3. Foundation/5a. Task 1/5a. Task 1/5a. Task 1/ViewController.swift
@@ -9,25 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let customQueue = OperationQueue()
+    let runButton = UIButton(type: .system)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let customQueue = OperationQueue()
-        let operation1 = BlockOperation{
-            self.heavyOperation()
-        }
-        customQueue.addOperations([operation1], waitUntilFinished: false)
-        
-        OperationQueue.main.addOperation {
-            self.heavyOperation()
-        }
+        setupUI()
     }
     
-    func heavyOperation() {
+    func setupUI() {
+        view.backgroundColor = .systemBackground
+        
+        runButton.setTitle("Run heavy task", for: .normal)
+        runButton.frame = CGRect(origin: .zero, size: CGSize(width: 400, height: 100))
+        runButton.center = view.center
+        runButton.addTarget(self, action: #selector(runButtonTapped), for: .touchUpInside)
+        view.addSubview(runButton)
+    }
+    
+    @objc func runButtonTapped() {
+        OperationQueue.main.addOperation {
+            self.heavyOperationA()
+        }
+        
+        let backgroundOperation = BlockOperation {
+            self.heavyOperationB()
+        }
+        customQueue.addOperation(backgroundOperation)
+    }
+    
+    func heavyOperationA() {
         print("Operation \"A\" started")
-        for _ in 0..<1000000 {
+        for _ in 0..<100000000 {
             // do nothing
         }
         print("Operation \"A\" finished")
     }
+    
+    func heavyOperationB() {
+        print("Operation \"B\" started")
+        for _ in 0..<100000000 {
+            // do nothing
+        }
+        print("Operation \"B\" finished")
+    }
 }
-
