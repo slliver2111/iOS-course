9e3c98a8459974fbf7927d346a26a852aef8b1ad
arturbednarz <slliver@icloud.com>
2025-08-07 22:51:17 +0200

    SwiftUI: task 5

---
+++ 4. UISwift/2e. Task 5/Task 5/Task 5/ContentView.swift
@@ -0,0 +1,51 @@
+//
+//  ContentView.swift
+//  Task 5
+//
+//  Created by Artur Bednarz on 07/08/2025.
+//
+ 
+import SwiftUI
+
+struct CardView<Content: View>: View {
+    let title: String
+    @ViewBuilder let content: () -> Content
+    
+    var body: some View {
+        VStack(spacing: 8) {
+            Text(title)
+                .font(.title)
+            content()
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        VStack(spacing: 40) {
+            CardView(title: "Card 1") {
+                HStack {
+                    Button("Ok") {}
+                    Button("Cancel") {}
+                }
+            }
+            CardView(title: "Card 2") {
+                HStack {
+                    Image(systemName: "circle.fill")
+                        .foregroundColor(Color.red)
+                    Image(systemName: "square.fill")
+                        .foregroundColor(Color.green)
+                    Image(systemName: "triangle.fill")
+                        .foregroundColor(Color.yellow)
+                }
+            }
+            CardView(title: "Card 3") {
+                Text("Some example text")
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 4. UISwift/2e. Task 5/Task 5/Task 5/Task_5App.swift
@@ -0,0 +1,17 @@
+//
+//  Task_5App.swift
+//  Task 5
+//
+//  Created by Artur Bednarz on 07/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct Task_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
