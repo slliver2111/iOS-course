7b341d9cf9b3ec33f368dc17d82b07f05d79f4aa
arturbednarz <slliver@icloud.com>
2025-08-08 12:51:15 +0200

    SwiftUI: task 7 fix

--- 4. UISwift/2g. Task 7/Task 7/Task 7/ContentView.swift
+++ 4. UISwift/2g. Task 7/Task 7/Task 7/ContentView.swift
@@ -43,8 +43,6 @@ struct ContentView: View {
                             .foregroundStyle(.gray)
                     }
                 }
-                Toggle("isPremium", isOn: $myExample.isPremium)
-                    .toggleStyle(.switch)
             }
         }
         .padding()
