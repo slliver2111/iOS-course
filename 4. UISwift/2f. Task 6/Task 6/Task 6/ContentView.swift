103c433b986baa7e0682a559c7b6cb47ffe71fe6
arturbednarz <slliver@icloud.com>
2025-08-08 12:13:58 +0200

    SwiftUI: task 6

---
+++ 4. UISwift/2f. Task 6/Task 6/Task 6/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  Task 6
+//
+//  Created by Artur Bednarz on 07/08/2025.
+//
+
+import SwiftUI
+
+struct Product: Identifiable {
+    var id = UUID()
+    let name: String
+    var inBasket: Bool
+}
+
+struct ContentView: View {
+    @State private var listOfProducts = [
+        Product(name: "Apple", inBasket: false),
+        Product(name: "Pear", inBasket: false),
+        Product(name: "Banana", inBasket: false),
+        Product(name: "Egg", inBasket: true),
+        Product(name: "Tuna", inBasket: false),
+    ]
+    
+    var body: some View {
+        NavigationStack {
+            List($listOfProducts) { $product in
+                HStack {
+                    Toggle(product.name, isOn: $product.inBasket)
+                }
+            }
+            .navigationTitle("Grocery List")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
