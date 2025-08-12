99dc54f2938e50b123f96ee54f1cbab62ec146c6
arturbednarz <slliver@icloud.com>
2025-08-12 11:02:59 +0200

    SwiftUI, navigation, task 2

---
+++ 4. UISwift/3h. Task 2N/Task 2N/Task 2N/ContentView.swift
@@ -0,0 +1,59 @@
+//
+//  ContentView.swift
+//  Task 2N
+//
+//  Created by Artur Bednarz on 12/08/2025.
+//
+
+import SwiftUI
+
+struct SettingsRoute: Hashable { let id: UUID }
+struct ProfileRoute: Hashable { let id: UUID }
+
+struct HomeView: View {
+    @State private var path = NavigationPath()
+    
+    private let settingsRoute = SettingsRoute(id: UUID())
+    
+    var body: some View {
+        NavigationStack(path: $path){
+            Button("Go to Settings") { path.append(settingsRoute) }
+                .navigationDestination(for: SettingsRoute.self) {_ in
+                    SettingsView(path: $path)
+                }
+                .navigationDestination(for: ProfileRoute.self) {_ in
+                    ProfileView(path: $path)
+                }
+                .navigationTitle("Home")
+        }
+    }
+}
+
+struct SettingsView: View {
+    @Binding var path: NavigationPath
+    
+    private let profileRoute = ProfileRoute(id: UUID())
+    
+    var body: some View {
+        Button("Go to Profile") {
+            print(path)
+            print("test1")
+            path.append(profileRoute)
+        }
+        .navigationTitle("Settings")
+    }
+}
+
+struct ProfileView: View {
+    @Binding var path: NavigationPath
+    
+    var body: some View {
+        Button("Go to Home") { path = .init() }
+            .navigationTitle("Profile")
+    }
+}
+
+
+#Preview {
+    HomeView()
+}
---
+++ 4. UISwift/3h. Task 2N/Task 2N/Task 2N/Task_2NApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_2NApp.swift
+//  Task 2N
+//
+//  Created by Artur Bednarz on 12/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct Task_2NApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
